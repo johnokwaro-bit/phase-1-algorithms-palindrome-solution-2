7708db4bce1cf538ea42a7f942cd982282c104c6
John Okwaro <john.okwaro@moringaschool.com>
2022-08-28 02:06:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  //iterate from the beginning to the middle
+  for(let i = 0; i<word.length/2; i++){
+    //check each letter to the corresponding letter from the end
+    const j = word.length-1-i;
+    if( word[i] !== word[j]) return false;
+  }
+
+  return true;
 }
 
 /* 
   Add your pseudocode here
 */
-
+//the first letter should be equal to the last letter 
+//and the second letter equal to the second last letter
+//the trend continues to the middle
+//iterate from the beginning to the middle
+//check each letter to the corresponding letter from the end
+//if any of the pairs don't match return false
+//otherwise return true
 /*
   Add written explanation of your solution here
 */
